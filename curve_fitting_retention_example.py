641c50c8f872a6f77a85835368205c508b8eca59
Jak Marshalll <JMarshall@sega.co.uk>
2016-11-08 18:21:07 +0000

    Aesthetic Sugar for Curve Fitting Tutorial

--- curve_fitting_retention_example.py
+++ curve_fitting_retention_example.py
@@ -6,21 +6,21 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-### Read in data ###
+### Read in Data ###
 
 xdata = np.array([1,3,7,14])
 ydata = np.array([0.38,0.22,0.17,0.12])
 
-### Define concept type = power functions ###
+### Define Concept Type = Power Functions ###
 
 def curve(x, a, b, c):
     return a * np.exp(-b * x) + c
 
-### Fit that curve! ###
+### Fit that Curve! ###
 
 popt, pcov = curve_fit(curve, xdata, ydata)
 
-### Get the new curve ###
+### Get the New Curve ###
 
 xcurve = np.linspace(1,30,30)
 ycurve = curve(xcurve, popt[0],popt[1],popt[2])
